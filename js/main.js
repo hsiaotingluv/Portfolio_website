6917a1f98b1533fb2bd88e72ce1dd976ccb08df2
Chen Hsiao Ting <hsiaoting0919@gmail.com>
2022-01-28 02:16:18 +0800

    Add portfolio

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ window.addEventListener("scroll", function(){
 	header.classList.toggle("sticky", window.scrollY > 0);
 })
 
-//Portfolio section - Modal
+//Services section - Modal
 const serviceModals = document.querySelectorAll(".service-modal");
 const learnmoreBtns = document.querySelectorAll(".learn-more-btn");
 const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
@@ -23,6 +23,29 @@ modalCloseBtns.forEach((modalCloseBtns) => {
 	modalCloseBtns.addEventListener("click", () => {
 		serviceModals.forEach((modalView) => {
 			modalView.classList.remove("active");
+		});
+	});
+});
+
+//Portfolio section - Modal
+const portfolioModals = document.querySelectorAll(".portfolio-model");
+const imgCards = document.querySelectorAll(".img-card");
+const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn");
+
+var portfolioModal = function(modalClick){
+	portfolioModals[modalClick].classList.add("active");
+}
+
+imgCards.forEach((imgCard, i) => {
+	imgCard.addEventListener("click", () => {
+		portfolioModal(i);
+	});
+});
+
+portfolioCloseBtns.forEach((portfolioCloseBtn) => {
+	portfolioCloseBtn.addEventListener("click", () => {
+		portfolioModals.forEach((portfolioModalView) => {
+			portfolioModalView.classList.remove("active");
 		});
 	});
 });
