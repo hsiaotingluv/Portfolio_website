afcc9afa4b1d3fc83d2fe9b89996d2ca7981ecab
Chen Hsiao Ting <hsiaoting0919@gmail.com>
2022-01-30 16:23:29 +0800

    Add scroll to top

--- js/main.js
+++ js/main.js
@@ -68,8 +68,36 @@ var swiper = new Swiper(".testimonial-swiper", {
 //Website dark/light theme
 
 //Scroll to top button
+const scrollTopBtn = document.querySelector(".scrollToTop-btn");
+
+window.addEventListener("scroll", function(){
+	scrollTopBtn.classList.toggle("active", window.scrollY > 500);
+});
+
+scrollTopBtn.addEventListener("click", () => {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+})
 
 //Navigation menu items active on page scroll
+window.addEventListener("scroll", () => {
+	const sections = document.querySelectorAll("section");
+	const scrollY = window.pageYOffset;
+	
+	sections.forEach(current => {
+		let sectionHeight = current.offsetHeight;
+		let sectionTop = current.offsetTop - 50;
+		let id = current.getAttribute("id");
+		
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active");
+		}
+		else{
+			document.querySelector(".nav-items a[href*=" + id + "]").classList.remove("active");
+		}
+	});
+});
+
 
 //Responsive navigation menu toggle
 
