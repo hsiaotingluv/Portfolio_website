7b289537124c31043466d44b2b06293043fb919d
Chen Hsiao Ting <hsiaoting0919@gmail.com>
2022-01-30 15:18:44 +0800

    Add testimonial

--- js/main.js
+++ js/main.js
@@ -50,7 +50,20 @@ portfolioCloseBtns.forEach((portfolioCloseBtn) => {
 	});
 });
 
-//Our clients - Swiper
+//Testimonials - Swiper
+var swiper = new Swiper(".testimonial-swiper", {
+	slidesPerView: 1,
+	spaceBetween: 30,
+	loop: true,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
 
 //Website dark/light theme
 
